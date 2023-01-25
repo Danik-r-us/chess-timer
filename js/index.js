0b4a9f6840eacd0d0f73d9add98f3362ec03f5dc
Danik-r-us <Daniilsolokha@gmail.com>
2023-01-25 18:38:30 -0500

    Worked on timer function and some style changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 //Starting Variables
+let startButton = document.querySelector('#start')
 let button1 = document.getElementById('button1')
 let button2 = document.getElementById('button2')
 let time1 = document.getElementById('time1')
@@ -6,13 +7,15 @@ let time2 = document.getElementById('time2')
 let readyPlayers = false
 let timerRunningForPlayer1 = false
 let timerRunningForPlayer2 = false
+let player1IntervalID
+let player2IntervalID
 let minutes
 let seconds
 let centiseconds
+let standardNotation = "${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}"
 
 //Play button must be enabled for timers to run
 let start = function(){
-    let startButton = document.querySelector('#start')
     if(startButton.innerHTML == '⏵'){
         startButton.innerHTML = '⏸'
         readyPlayers = true
@@ -24,14 +27,23 @@ let start = function(){
     }
 }
 
+//10sec button sets both timers to 10sec
+let setTime10Sec = function(){
+    minutes = 0
+    seconds = 10
+    centiseconds = 0
+    time1.innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
+    time2.innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
+    console.log(`${standardNotation}`)
+}
+
 //5 min button sets both timers to 10
 let setTime5Min = function(){
     minutes = 5
     seconds = 0
     centiseconds = 0
-    document.querySelector('#time1').innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
-    document.querySelector('#time2').innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
-    console.log('5min')
+    time1.innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
+    time2.innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
 }
 
 //10 min button sets both timers to 10
@@ -39,69 +51,71 @@ let setTime10Min = function(){
     minutes = 10
     seconds = 0
     centiseconds = 0
-    document.querySelector('#time1').innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
-    document.querySelector('#time2').innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
-    console.log('10min')
+    time1.innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
+    time2.innerText=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
 }
 
 //Player 1 Controls, first press of the game starts player 1's timer, second press stops player 1's timer and starts player 2's timer
 let startCountDownForPlayer1 = function(){
-    function timer(){
-        setInterval(function(){
-        if(centiseconds < 0){
-            centiseconds += 99
-            seconds--
-        } if(seconds < 0){
-            seconds += 60
-            minutes--
-        }
-        currentTime=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
-        time1.innerHTML = currentTime
-        centiseconds--
-    },10)
-    currentTime = time1.innerText
-    }
-
     if(timerRunningForPlayer1 == false && timerRunningForPlayer2==false && readyPlayers==true){
-        timerRunningForPlayer2 = true
-        timer()
-    }else if(timerRunningForPlayer1 == false && timerRunningForPlayer2 == true && readyPlayers==true){
-        timerRunningForPlayer2 = false
-        clearInterval(timer())
+        timerRunningForPlayer1 = true
+        player1IntervalID = setInterval(function(){
+            if(centiseconds < 0){
+                centiseconds += 99
+                seconds--
+            } if(seconds < 0){
+                seconds += 60
+                minutes--
+            }
+            time1.innerHTML=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
+            centiseconds--
+            if(time1.innerHTML == `0:00.00`){
+                clearInterval(player1IntervalID)
+            }
+            if(readyPlayers==false){
+                clearInterval(player1IntervalID)
+            }
+        },10)
+    }else if(timerRunningForPlayer1 == true && timerRunningForPlayer2 == false && readyPlayers==true){
+        timerRunningForPlayer1 = false
+        clearInterval(player1IntervalID)
+        startCountDownForPlayer2()
     }
 }
 
 //Player 2 Controls, first press of the game starts player 2's timer, second press stops player 2's timer and starts player 1's timer
 let startCountDownForPlayer2 = function (){
-
-    function timer(){
-        setInterval(function(){
-        if(centiseconds < 0){
-            centiseconds += 99
-            seconds--
-        } if(seconds < 0){
-            seconds += 60
-            minutes--
-        }
-        currentTime=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
-        time2.innerHTML = currentTime
-        centiseconds--
-    },10)
-    currentTime = time2.innerText
-    }
-
     if(timerRunningForPlayer1 == false && timerRunningForPlayer2==false && readyPlayers==true){
         timerRunningForPlayer2 = true
-        timer()
+        player2IntervalID = setInterval(function(){
+            if(centiseconds < 0){
+                centiseconds += 99
+                seconds--
+            } if(seconds < 0){
+                seconds += 60
+                minutes--
+            }
+            time2.innerHTML=`${minutes}:${seconds<10 ? '0'+seconds:seconds}.${centiseconds<10?'0'+centiseconds:centiseconds}`
+            centiseconds--
+            if(time2.innerHTML == `0:00.00`){
+                clearInterval(player2IntervalID)
+            }
+            if(readyPlayers==false){
+                clearInterval(player2IntervalID)
+            }
+        },10)
     }else if(timerRunningForPlayer1 == false && timerRunningForPlayer2 == true && readyPlayers==true){
         timerRunningForPlayer2 = false
-        clearInterval(timer())
+        clearInterval(player2IntervalID)
+        startCountDownForPlayer1()
     }
 }
 
+
 //Event listeners
+document.querySelector('#tenSecond').addEventListener('pointerdown',setTime10Sec)
 document.querySelector('#min5').addEventListener('pointerdown', setTime5Min)
 document.querySelector('#min10').addEventListener('pointerdown', setTime10Min)
 document.querySelector('#start').addEventListener('pointerdown', start)
-document.querySelector('#button1').addEventListener('pointerdown', startCountDownForPlayer1)
-document.querySelector('#button2').addEventListener('pointerdown', startCountDownForPlayer2)
+document.querySelector('#time1').addEventListener('pointerdown', startCountDownForPlayer1)
+document.querySelector('#time2').addEventListener('pointerdown', startCountDownForPlayer2)
